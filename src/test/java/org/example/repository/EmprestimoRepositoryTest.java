84bd94d372e72fe0903bb70359f4e114789ca606
Andre Batista Bezerra <andre3901@hotmail.com>
2026-07-10 09:14:04 -0300

    Commit com a criação dos testes de usuario e repositorio

---
+++ src/test/java/org/example/repository/EmprestimoRepositoryTest.java
@@ -0,0 +1,20 @@
+package org.example.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmprestimoRepositoryTest {
+
+    @Test
+    void salvar() {
+    }
+
+    @Test
+    void buscarEmprestimoAtivo() {
+    }
+
+    @Test
+    void devolver() {
+    }
+}
---
+++ src/test/java/org/example/repository/UsuariosRepositoryTest.java
@@ -0,0 +1,16 @@
+package org.example.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuariosRepositoryTest {
+
+    @Test
+    void salvar() {
+    }
+
+    @Test
+    void excluir() {
+    }
+}
